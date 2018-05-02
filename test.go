91b9b988f4af447c1acacadc35c794d18629c165
ChenGefei <42648498@qq.com>
2018-05-02 06:39:47 -0700

    learning golang

--- test.go
+++ test.go
@@ -1,22 +1,32 @@
 package main
-
+  
 import (
-	"fmt"
+        "fmt"
 )
 
 func main() {
-	p := newInt()
-	q := newInt()
-	fmt.Println(*p,*q, p, q)
-	fmt.Println(p == q)
-	var pp *int
-	*p = 100
-	*q = 100
-	fmt.Println(*p,*q, p, q)
-	fmt.Println(p == q)
-	fmt.Println(pp == p)
+        x := 75
+        y := 50
+        fmt.Println("gcd", gcd(x,y))
+        fmt.Println("fib", fib(x))
+
+	medals := []string{"gold", "silver", "bronze"}
+	fmt.Println(medals)
 }
 
-func newInt() *int {
-	return new(int)
+func gcd(x, y int) int {
+        fmt.Printf("gcd start:[%d][%d]\n", x, y)
+	for y != 0 {
+                x, y = y, x%y
+	}
+        return x
 }
+
+func fib(n int) int {
+        x,y := 0,1
+        for i:= 0; i<n; i++ {
+                x,y = y, x+y
+        }
+        return x
+}
+
