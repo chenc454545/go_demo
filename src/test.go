add5eace2a3c876e42428cde82232dbfab20e2ec
ChenGefei <442648498@qq.com>
2018-05-07 08:21:43 -0700

    learning golang

--- src/test.go
+++ src/test.go
@@ -1,11 +1,13 @@
+// 3.1
 package main
 
-import "fmt"
-import "simplemath"
+import (
+	"fmt"
+)
 
-//test 2.6
-func main() {
-	a := 1
-	b := 2
-	fmt.Println(sm.Add(a,b))
+func main () {
+	var u uint8 = 255
+	fmt.Println(u, u+1, u*u)
+	var i int8 = 127
+	fmt.Println(i, i+1, i*i)
 }
